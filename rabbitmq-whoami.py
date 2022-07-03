00d5ce06b3a9f7dce93e59c22fd5d1ad2a070080
Inderjeet Singh <singhinder1208@gmail.com>
2022-07-03 20:41:28 +0530

    Fixed auth and cookies

--- rabbitmq-whoami.py
+++ rabbitmq-whoami.py
@@ -1,12 +1,34 @@
 import requests
 import sys
+#import logging  # remove this for debugging purposes
+from requests.auth import HTTPBasicAuth
+
+headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) Chrome/103.0.0.0 Mobile','content-type':'application/json'}
+cookies = dict(m='2258:Z3Vlc3Q6Z3Vlc3Q=')
+basic = HTTPBasicAuth('guest', 'guest')
 
 def get_status(url):
-    headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36', 'content-type': 'application/json', 'authorization': 'Basic Z3Vlc3Q6Z3V0ZXN0', 'Cookie': 'm=2258:Z3Vlc3Q6Z3V0ZXN0', 'Host': 'alert-spider.rmq.cloudamqp.com'}
-    r = requests.get(url + '/api/whoami')
-    print(url, r.status_code)
+    r = requests.get(url + '/api/whoami', headers=headers, cookies=cookies, auth=basic)
+    print(url, r.status_code, r.text)
 
-file = open('test.txt', 'r')
-for url in file:
-    url = url.rstrip('\n');
-    get_status(url)
+# remove """ for debugging purposes
+"""
+try:
+    import http.client as http_client
+except ImportError:
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+"""
+
+if __name__  == "__main__":
+    file = open(sys.argv[1], 'r')
+    
+    for url in file:
+        url = url.rstrip('\n');
+        get_status(url)
